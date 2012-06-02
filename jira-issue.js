80384fdc46d36135b0f93640a05edf0d6b32e028
mikelietz <mike@internetaddress.com>
2012-06-02 01:01:37 -0300

    Update for new nerdie API. Update to actually contact JIRA, but this requires considerable configuration.
    Sample config.json for this new version:
    
       "plugins": {
         "jira_issue": {
           "prefix": [ "TEST", "OARS", "DELIVERY" ],
           "hostname": "acerdev.onjira.com",
           "path": "/rest/api/2.0.alpha1/",
           "username": "username",
           "password": "password",
           "issue_base_url": "https://acerdev.onjira.com/browse/"
         }
    
    Note this does not handle nonexistent issues yet.

--- jira-issue.js
+++ jira-issue.js
@@ -1,24 +1,67 @@
-var NerdieInterface = require('nerdie_interface.js')
-  , nerdie = null
+var https = require('https');
+
+var nerdie = null
   , prefix = null
   , config = null
   , baseurl = null;
 
 function Jira_Issue(parentNerdie) {
 	config = (parentNerdie.config.plugins.jira_issue) ? parentNerdie.config.plugins.jira_issue : {};
-	this.pluginInterface = new NerdieInterface(parentNerdie, this);
+	this.pluginInterface = new parentNerdie.iface(parentNerdie, this);
 	nerdie = parentNerdie;
 }
 
 Jira_Issue.prototype.init = function () {
-	var pattern = RegExp( "^(" + config.prefix + "-|-)(\\d+)$" );
+	var prefixes = config.prefix.join('|');
+	var pattern = RegExp( "^(" + prefixes + ")-(\\d+)$", "i" );
 	this.pluginInterface.registerPattern( pattern,
 		this.jira_issue
 	);
 };
 
 Jira_Issue.prototype.jira_issue = function(msg) {
-	msg.say( config.baseurl + config.prefix + '-' + msg.match_data[2] );
+	var username = config.username,
+		password = config.password,
+		api_url = config.api_url,
+		issue = msg.match_data[0].toUpperCase(),
+		issue_url = config.issue_base_url + issue;
+
+	var options = {
+		hostname: config.hostname,
+		path: config.path + "issue/" + issue,
+		auth: username + ":" + password
+	};
+
+//	console.log( options );
+
+	var req = https.request( options, function( res ) {
+//		console.log("statusCode: ", res.statusCode);
+//		console.log("headers: ", res.headers);
+
+		res.on('data', function(d) {
+//			process.stdout.write(d);
+			try {
+				var parsed_data = JSON.parse( d );
+// console.log( parsed_data.fields );
+// console.log( parsed_data.fields );
+// console.log( "=============================================");
+// console.log( parsed_data.fields.summary);
+// console.log( parsed_data["status"] );
+				msg.say( issue + " Status: " + parsed_data.fields.status.value.name  );
+				msg.say( "Summary: " + parsed_data.fields.summary.value );
+				msg.say( "Issue URL: " + issue_url + " API URL: " + parsed_data.self );
+			} catch (e) {
+				msg.say( "Could not parse response." );
+				return;
+			}
+		});
+
+	});
+	req.end();
+
+	req.on( 'error', function(e) {
+		msg.say( "There was an error. " + e );
+	});
 };
 
 module.exports = Jira_Issue;
